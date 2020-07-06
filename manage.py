b79f60938e8d5a5244aea79f82ca0e5d4fcd16e6
Ubayd Bapoo <ubayd.bapoo@gmail.com>
2020-07-06 10:12:07 +0200

    variable renamed

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'github.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'github_auth.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
